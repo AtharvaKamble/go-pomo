c094f53fd4d79d2e69dec5fe35071288971693f0
AtharvaKamble <atharvadkamble00@gmail.com>
2024-07-03 01:14:12 -0400

    add ticking timer go routine

--- internal/models/timer.go
+++ internal/models/timer.go
@@ -14,7 +14,9 @@ var (
 	cacheOnce sync.Once
 )
 
+// GetTimerInstance creates a single new instance utilizing the Singleton pattern
 func GetTimerInstance() *Timer {
+	// Do only run once on every sync.Once instance
 	cacheOnce.Do(func() {
 		cache = &Timer{Start: time.Now()}
 	})
@@ -22,10 +24,12 @@ func GetTimerInstance() *Timer {
 	return cache
 }
 
+// GetDelta provides the time elapsed from the creation of the timer *Timer struct instance
 func (timer *Timer) GetDelta() time.Duration {
 	return time.Since(timer.Start)
 }
 
+// Tick is a GoRoutine that ticks every second for a given duration
 func (timer *Timer) Tick(duration time.Duration, tickChannel chan time.Duration) {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()

a2e6afa383def64cb22ad5947d959dc15870b027
AtharvaKamble <atharvadkamble00@gmail.com>
2024-07-03 01:15:07 -0400

    [feat] [beta v0.0.1] add pages navigation

--- cmd/pomodoro/main.go
+++ cmd/pomodoro/main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"github.com/rivo/tview"
 	"pomodoro/internal/models"
+	"pomodoro/internal/ui"
 	"time"
 )
+
+var (
+	app         *tview.Application
+	textView    *tview.TextView
+	currentPage string
+)
+
+const pageCount = 2
 
 func main() {
 
 	timer := models.GetTimerInstance()
 
 	delta := make(chan time.Duration)
-	go timer.Tick(10*time.Second, delta)
 
-	for t := range delta {
-		fmt.Println(t)
+	total := 1500 * time.Second
+
+	app = tview.NewApplication()
+	// box := tview.NewBox().SetBorder(true).SetTitle("GoPomo")
+
+	// there must be global context to store which page was selected
+
+	currentPage = "Landing"
+	pages := ui.SetupPages(app, &currentPage)
+
+	textView = tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+
+	go func() {
+		for t := range delta {
+			curTime := total - t.Truncate(time.Second)
+
+			app.QueueUpdateDraw(func() {
+				textView.SetText(curTime.String())
+			})
+		}
+
+	}()
+
+	go timer.Tick(total, delta)
+
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
 	}
+
 }
